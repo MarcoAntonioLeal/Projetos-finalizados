e3cfa8b92816ea58cad50739a187463ec9cdd0f9
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2023-08-24 23:57:37 -0300

    atualizando projeto

--- projeto_jogo_da_velha/script.js
+++ projeto_jogo_da_velha/script.js
@@ -2,30 +2,32 @@ const player1 = document.getElementById('namePlayerOne')
 const player2 = document.getElementById('namePlayerTwo')
 const inputName = document.getElementById('name')
 const btnConfirm = document.getElementById('confirm')
+const btnRestart = document.getElementById('restart')
+
 let players = []
 
-// name of players for click mouse
-document.getElementById('confirm').addEventListener('click', function() {
+btnConfirm.addEventListener('click', function() {
 
     players.push(inputName.value)
     player1.innerText = players[0]
     player2.innerText = players[1]
 
-    if(player1.innerText == '' || player2.innerText == '' ) {
+    if (player1.innerText == '' || player2.innerText == '') {
         alert('Preencha o nome dos jogadores!')
     }
 
     if (player1.innerText == players[0] && player2.innerText == players[1]) {
-        inputName.value = ''
-        inputName.setAttribute('placeholder', '')
-        inputName.disabled = 'true'
-        btnConfirm.disabled = 'true'
-        inputName.classList.add('disabled')
+        btnConfirm.classList.add('none')
+        inputName.classList.add('none')
     } else {
         inputName.value = ''
         inputName.setAttribute('placeholder', 'jogador 2')
         inputName.focus()
         player2.innerText = ''
     }
+})
+
+btnRestart.addEventListener('click', function(ev) {
+    location.reload()
 })
 
