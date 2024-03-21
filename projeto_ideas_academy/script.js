0da8a76102a528547aa5ffce8624dd35837d18bc
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-03-20 23:31:34 -0300

    atualizando projeto

--- projeto_ideas_academy/script.js
+++ projeto_ideas_academy/script.js
@@ -4,12 +4,15 @@ const email = document.querySelector('#email')
 const telefone = document.querySelector('#telefone')
 const xpAdc = document.querySelector('#infoAdicionais')
 
-const areaAtuacao = document.querySelectorAll('#area')
-areaAtuacao.forEach((element) => {
+let areaAtuacao = ''
+document.querySelectorAll('#area').forEach((element) => {
     element.addEventListener('click', (evento) => {
-        evento.currentTarget.value
+        areaAtuacao += evento.currentTarget.value
     })
 })
+
+let idiomas = ''
+//document.querySelectorAll('#')
 
 
 //botão
@@ -51,8 +54,15 @@ botao.addEventListener('click', () => {
     listaTelefone.innerText = 'Telefone: ' + telefone.value
 
     ulPrincipal.appendChild(listaExperiencia)
-    listaExperiencia.innerText = 'Experiência profissional: ' + xpAdc
+    listaExperiencia.innerText = 'Experiência profissional: ' + xpAdc.value
 
     ulPrincipal.appendChild(listaAtuacao)
+    listaAtuacao.innerText = 'Área de atuação: ' + areaAtuacao
+
+
     ulPrincipal.appendChild(listaLinguas)
+
+    nome.value = ''
+    email.value = ''
+    telefone.value = ''
 })
