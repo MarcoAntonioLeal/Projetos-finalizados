e95c4df957527a8baa8bd7a447885d7246b85283
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-04-24 21:13:13 -0300

    atualizando prejeto

--- projetoAPI_IdeasAcademy/script.js
+++ projetoAPI_IdeasAcademy/script.js
@@ -1,71 +1,80 @@
-//entradas do input
+ const forecastImg = document.querySelector('#imgForecast') 
+
 const search = document.querySelector('#search')
 const btnSearch = document.querySelector('#btnSearch')
 
-//entradas da previsão do tempo
 const city = document.querySelector('#city')
+const forecast = document.querySelector('#forecast')
 const tempMax = document.querySelector('#tempMax')
 const tempMin = document.querySelector('#tempMin')
-const forecast = document.querySelector('#forecast')
-const forecastImg = document.querySelector('#imgForecast')
+const thermal = document.querySelector('#thermal')
 const rain = document.querySelector('#rain')
 const humidity = document.querySelector('#humidity')
+const pressure = document.querySelector('#pressure')
+const cloudiness = document.querySelector('#cloudiness')
+const visibility = document.querySelector('#visibility')
 const wind = document.querySelector('#wind')
-
-//resultado da previsão
-function resultForecast(dados) {
-    if(!dados.name) {
-        alert('Essa cidade não existe')
-        location.reload()
-    }
-    city.innerText = dados.name
-    forecastImg.src = imageForecast(dados.weather[0].icon) 
-    tempMax.innerText = Number(dados.main.temp_max).toFixed(0) + '°C'
-    tempMin.innerText = Number(dados.main.temp_min).toFixed(0) + '°C'
-    forecast.innerText = dados.weather[0].description
-    rain.innerText = dados.clouds.all + '%'
-    humidity.innerText = dados.main.humidity + '%'
-    wind.innerText = dados.wind.speed + 'm/s'
-}
+const direcWind = document.querySelector('#direcWind')
+const latitude = document.querySelector('#latitude')
+const longitude = document.querySelector('#longitude')
 
 function imageForecast(icon) {
     return `img/iconPrevisaoTempo/campoPrevisao/${icon}.png`
 }
 
-//evento e API
-btnSearch.addEventListener('click', () => {
-    weatherForecast(search.value)
-    search.value = ''
-})
-
 async function weatherForecast(city) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'ba438284fa602652dd54444ff2b38918'}&lang=pt_br&units=metric`).then(result => result.json())
-
+    
     resultForecast(dados)
 }
 
+function resultForecast(dados) {
+    if(!dados.name) {
+        alert('Essa cidade não existe')
+        location.reload()
+    }
+    console.log(dados)
+    city.innerText = dados.name
+    forecastImg.src = imageForecast(dados.weather[0].icon) 
+    tempMax.innerText = Number(dados.main.temp_max).toFixed(0) + '°C'
+    tempMin.innerText = Number(dados.main.temp_min).toFixed(0) + '°C'
+    thermal.innerText = Number(dados.main.feels_like).toFixed(0) + '°C'
+    cloudiness.innerText = dados.clouds.all + '%'
+    rain.innerText = dados.clouds.all + '%'
+    humidity.innerText = dados.main.humidity + '%'
+    pressure.innerText = dados.main.pressure + 'hPa'
+    visibility.innerText = Number(dados.visibility / 1000).toFixed(0) + 'km'
+    forecast.innerText = dados.weather[0].description
+    wind.innerText = dados.wind.speed + 'm/s'
+    direcWind.innerText = dados.wind.deg + '°'
+    latitude.innerText = Number(dados.coord.lat).toFixed(2) + '°'
+    longitude.innerText = Number(dados.coord.lon).toFixed(2) + '°' 
+}
+
+btnSearch.addEventListener('click', () => {
+    weatherForecast(search.value)
+    iframeMaps.src = locationCity(search.value)
+    search.value = ''
+    search.focus()
+})
+
+document.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter' && search.value != 0) {
+        btnSearch.click()
+    }
+})
+
+//footer
 const timeApplication = document.querySelector('.time')
 const dateApplication = document.querySelector('.date')
 
 function time() {
-    const hour = new Date().getHours()
-    const minutes = new Date().getMinutes()
-
-    if(hour == 0) {
-       return `${hour}0 : ${minutes}`
-    } else {
-        return `${hour} : ${minutes}` 
-    }
+    return new Date().toLocaleTimeString().slice(0, 5)
 }
 
 function date() {
-    const day = new Date().getDate()
-    const month = new Date().getMonth() + 1
-    const year = new Date().getFullYear()
-    return `${day} / ${month} / ${year}`
+    return new Date().toLocaleDateString()
 }
 
 timeApplication.innerText = time()
 dateApplication.innerText = date()
-
-
