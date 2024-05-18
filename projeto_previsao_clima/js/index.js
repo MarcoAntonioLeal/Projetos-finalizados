c518ce753db2d9793e7ab451ebd5bff8e167446e
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2024-05-18 16:03:40 -0300

    atualizando arquivos

--- projeto_previsao_clima/js/index.js
+++ projeto_previsao_clima/js/index.js
@@ -42,8 +42,6 @@ btnAuto.addEventListener('click', () => {
 
 
 //Main
-const btnHistory = document.querySelector('#history')
-
 const iframeMaps = document.getElementsByTagName('iframe')[0]  
 
 const forecastImg = document.querySelector('#imgForecast') 
@@ -98,24 +96,6 @@ function resultForecast(dados) {
     direcWind.innerText = dados.wind.deg + '°'
     latitude.innerText = Number(dados.coord.lat).toFixed(2) + '°'
     longitude.innerText = Number(dados.coord.lon).toFixed(2) + '°' 
-
-    const historyClimate = new climateHistory(
-    dados.name, 
-    dados.weather[0].icon, 
-    Number(dados.main.temp_max).toFixed(0) + '°C', 
-    Number(dados.main.temp_min).toFixed(0) + '°C', 
-    Number(dados.main.feels_like).toFixed(0) + '°C',
-    dados.clouds.all + '%',
-    dados.main.humidity + '%', 
-    dados.main.pressure + 'hPa',
-    Number(dados.visibility / 1000).toFixed(0) + 'km',
-    dados.weather[0].description,  
-    dados.wind.speed + 'm/s',
-    dados.wind.deg + '°',
-    Number(dados.coord.lat).toFixed(2) + '°',
-    Number(dados.coord.lon).toFixed(2) + '°')
-
-    arrayHistory.push(historyClimate)
 }
 
 btnSearch.addEventListener('click', () => {
@@ -130,33 +110,6 @@ document.addEventListener('keypress', (event) => {
         btnSearch.click()
     }
 })
-
-btnHistory.addEventListener('click', () => {
-    console.log(arrayHistory)
-})
-
-//histórico de pesquisa
-class climateHistory {
-    constructor(city, forecastImg, tempMax, tempMin, thermal, rain, humidity, pressure, visibility, forecast, wind, direcWind, latitude, longitude) {
-        this.date = date()
-        this.time = time()  
-        this.city = city
-        this.forecastImg = forecastImg
-        this.tempMax = tempMax
-        this.tempMin = tempMin
-        this.thermal = thermal
-        this.rain = rain
-        this.humidity = humidity
-        this.pressure = pressure
-        this.visibility = visibility
-        this.forecast = forecast
-        this.wind = wind 
-        this.direcWind = direcWind
-        this.latitude = latitude
-        this.longitude = longitude
-    }
-}
-const arrayHistory = []
 
 //footer
 const timeApplication = document.querySelector('.time')
