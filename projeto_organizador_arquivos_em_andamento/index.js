0248df78a8610e42b0bbf46b1ad46aa53494eec2
Marco Antonio S. Leal Filho <115421605+MarcoAntonioLeal@users.noreply.github.com>
2025-08-23 01:04:06 -0300

    atualizando projeto

--- projeto_organizador_arquivos_em_andamento/index.js
+++ projeto_organizador_arquivos_em_andamento/index.js
@@ -1,14 +1,45 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
-const dirOrganizador = fs.readdirSync('C:/Users/marc-/OneDrive/Desktop/Sayo', 'utf-8')
+const caminhoPastaOrganizadora = 'C:/Users/marc-/OneDrive/Desktop/Say_Watcher'
+const caminhoPastaFinal = 'C:/Users/marc-/OneDrive/Imagens/pasta_final'
 
-dirOrganizador.forEach(nomeArquivo => {
-    if (path.extname(nomeArquivo) !== '.ini') {
-        console.log(path.extname(nomeArquivo))
+const arqPastaOrganizadora = fs.readdirSync(caminhoPastaOrganizadora, 'utf-8')
+
+function moverArquivos() {
+    return fs.copyFileSync(
+            `${caminhoPastaOrganizadora}/${path.basename(arquivo)}`,
+            `${caminhoPastaFinal}/${path.extname(arquivo)}/${path.basename(arquivo)}`)
+}
+
+if(!fs.existsSync(caminhoPastaFinal)) {
+    fs.mkdirSync(caminhoPastaFinal)
+}
+
+arqPastaOrganizadora.forEach(arquivo => {
+    if(!fs.existsSync(`${caminhoPastaFinal}/${path.extname(arquivo)}`)) {
+        fs.mkdirSync(`${caminhoPastaFinal}/${path.extname(arquivo)}`)
+
+        /*fs.copyFileSync(
+            `${caminhoPastaOrganizadora}/${path.basename(arquivo)}`,
+            `${caminhoPastaFinal}/${path.extname(arquivo)}/${path.basename(arquivo)}`)*/
+    } else {
+        fs.copyFileSync(
+           /* `${caminhoPastaOrganizadora}/${path.basename(arquivo)}`,
+            `${caminhoPastaFinal}/${path.extname(arquivo)}/${path.basename(arquivo)}`)*/
     }
 })
 
+arqPastaOrganizadora.forEach(arquivo => {
+    console.log(path.basename(arquivo))
+})
+
+
+
+
+/*if(!dirOrganizador.length == 0) {
+   
+} */
 
 
 /*fs.watch('C:/Users/marc-/OneDrive/Desktop/Sayo', {persistent: true} ,(a, b) => {
